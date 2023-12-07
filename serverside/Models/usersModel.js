56472d8714b3cd5017f88f4b8a7eae13b8c6e259
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-12-07 08:47:52 +0530

    created a model for users

---
+++ serverside/Models/usersModel.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    default: function () {
+      return new ObjectId().toString();
+    },
+  },
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    match: [/\S+@\S+\.\S+/, "Please enter a valid email"],
+  },
+  gender: {
+    type: String,
+    enum: ["Male", "Female", "Gender Fluid"],
+  },
+  avatar: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^(http|https):\/\/[^ "]+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid URL!`,
+    },
+  },
+  domain: {
+    type: String,
+    enum: [
+      "Sales",
+      "Finance",
+      "Marketing",
+      "Management",
+      "UI Designing",
+      "Web Development",
+    ],
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
+const usersModel = mongoose.model("usersModel", userSchema);
+export default usersModel;
