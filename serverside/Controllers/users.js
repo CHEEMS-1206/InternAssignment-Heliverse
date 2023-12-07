91398741afe415e2b65079749b03ee79aca21822
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-12-07 08:48:37 +0530

    created controllers for different api calls to /api/users

---
+++ serverside/Controllers/users.js
@@ -0,0 +1,92 @@
+// Controllers for users '/' after URL => http://localhost:5000/api/users
+
+// importing model for rabbit
+import usersModel from "../Models/usersModel.js";
+
+// getting a particular user from the database by the given id
+export const getUser = async (req, res) => {
+  try {
+    const query = await usersModel.findById(req.params.id);
+    if (query) {
+      res.status(200).json(query);
+    } else {
+      res.status(400).json({
+        msg: "No users found with this id !",
+      });
+    }
+  } catch (e) {
+    res.status(400).json({ msg: e });
+  }
+};
+
+// getting list of all users in the database
+export const getAllUsers = async (req, res) => {
+  try {
+    const query = await usersModel.find();
+    if (query) {
+      res.status(200).json(query);
+    } else {
+      res.status(400).json({
+        msg: "Oops No user available !",
+      });
+    }
+  } catch (e) {
+    res.status(400).json({ msg: e });
+  }
+};
+
+// Adding a new user in the database
+export const postUser = async (req, res) => {
+  const newUser = new usersModel({
+    id: req.body.id,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    gender: req.body.gender,
+    avatar: req.body.avatar,
+    domain: req.body.domain,
+    available: req.body.available,
+  });
+  try {
+    const addingNewUser = await newUser.save();
+    res.status(200).json(newUser);
+  } catch (e) {
+    res.status(400).json({ msg: e });
+  }
+};
+
+// updating details for the user in the database with the id
+export const updateUser = async (req, res) => {
+  try {
+    const query = await usersModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      {
+        new: true,
+      }
+    );
+    if (!query) {
+      res.status(400).json({ msg: "No user exists with this id !!" });
+    } else {
+      res.status(200).json(query);
+    }
+  } catch (err) {
+    res.status(400).json({ msg: err });
+  }
+};
+
+// deleting user from the database with the given id
+export const deleteUser = async (req, res) => {
+  try {
+    const query = await usersModel.findByIdAndDelete(req.params.id);
+    if (!query) {
+      res.status(400).json({ msg: "No user exists with this id !!" });
+    } else {
+      res.status(200).json(query);
+    }
+  } catch (err) {
+    res.status(400).json({ msg: err });
+  }
+};
