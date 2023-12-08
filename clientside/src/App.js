e1030c2dd94f870e9e37f6894da22366ddf4a9da
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-12-08 17:37:44 +0530

    created a landing page that has userdata display, list of all users and navbar with logout button

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AddDetails from "./PageComponents/AddDetails.js";
+import LandingPage from "./PageComponents/LandingPage.js";
 import LoginPage from "./PageComponents/Loginpage.js";
 import RegisterPage from "./PageComponents/RegisterPage.js";
 
@@ -8,10 +9,11 @@ function App() {
   return (
     <Router>
       <Routes>
-      <Route exact path="login" element={<LoginPage/>} />
-      <Route path="/register" element={<RegisterPage/>} />
-      <Route path="/add-details" element={<AddDetails/>} />
-      <Route path="*" element={<div>Wrong Routes</div>} />
+        <Route exact path="login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/add-details" element={<AddDetails />} />
+        <Route path="/list-all-users" element={<LandingPage/> } />
+        <Route path="*" element={<div>Wrong Routes</div>} />
       </Routes>
     </Router>
   );
---
+++ clientside/src/PageComponents/LandingPage.js
@@ -0,0 +1,190 @@
+import {
+  MenuItem,
+  Select,
+  InputLabel,
+  FormControl,
+  TextField
+} from "@mui/material";
+
+import UserDetailsCard from "./UserDetailsCard";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function LandingPage() {
+  const [myDetails, setMyDetails] = useState(null);
+  const [allUsers, setAllUsers] = useState(null);
+  const [page, setPage] = useState(1);
+
+  const u_id = localStorage.getItem("u_id");
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchAllusers = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:5001/api/users?page=${page}`
+        );
+        if (response.ok) {
+          const listOfUsers = await response.json();
+          setAllUsers(listOfUsers);
+          console.log(listOfUsers);
+        } else {
+          // Handle error cases if needed
+          console.error("Error fetching user details:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
+    };
+    fetchAllusers();
+  }, [page]);
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      try {
+        const response = await fetch(`http://localhost:5001/api/users/${u_id}`);
+        if (response.ok) {
+          const userData = await response.json();
+          setMyDetails(userData);
+          console.log(userData);
+        } else {
+          // Handle error cases if needed
+          console.error("Error fetching user details:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
+    };
+    if (u_id) {
+      fetchUserDetails();
+    }
+  }, [u_id]);
+
+  const logOutHnadler = () => {
+    // Clear user-related data from localStorage
+    localStorage.removeItem("token");
+    localStorage.removeItem("u_id");
+    localStorage.removeItem("email");
+
+    // Show alert for successful logout
+    alert("Logged out successfully.");
+
+    // Redirect user to the login page
+    navigate("/login");
+  };
+  const prevHandler = () => {
+    if (page === 1) {
+      alert("You are at the starting page !");
+    } else {
+      setPage(page - 1);
+    }
+    console.log(page);
+  };
+
+  const nextHandler = () => {
+    if (!allUsers || page === allUsers.totalPages) {
+      alert("No more pages !");
+    } else {
+      setPage(page + 1);
+    }
+    console.log(page);
+  };
+  const handleChange = () => {
+    console.log("change");
+  };
+  const filterBy = () => {
+    console.log("change");
+  };
+
+  const gender = "Male";
+  const domain = "IT";
+  const available = true;
+  const username = "Alex"
+
+  return (
+    <div className="landingPage">
+      <div className="navbar">
+        <h1>Application Name</h1>
+        <button onClick={logOutHnadler}>LogOut</button>
+      </div>
+      <div className="container">
+        <div className="myProfile">
+          <UserDetailsCard user={myDetails} />
+        </div>
+        <div className="all-users">
+          <div className="filter">
+            <FormControl style={{ width: "22%" }} margin="normal" required>
+              <InputLabel>Gender</InputLabel>
+              <Select value={gender} name="gender" onChange={handleChange}>
+                <MenuItem value="">All Genders</MenuItem>
+                <MenuItem value="Male">Male</MenuItem>
+                <MenuItem value="Female">Female</MenuItem>
+                <MenuItem value="Agender">Agender</MenuItem>
+                <MenuItem value="Bigender">Bigender</MenuItem>
+                <MenuItem value="Polygender">Polygender</MenuItem>
+                <MenuItem value="Non-Binary">Non-Binary</MenuItem>
+                <MenuItem value="Gender-Fluid">Gender-Fluid</MenuItem>
+                {/* Add other gender options */}
+              </Select>
+            </FormControl>
+            <FormControl style={{ width: "22%" }} margin="normal" required>
+              <InputLabel>Domain</InputLabel>
+              <Select value={domain} name="domain" onChange={handleChange}>
+                <MenuItem value="">All Domains</MenuItem>
+                <MenuItem value="Sales">Sales</MenuItem>
+                <MenuItem value="Finance">Finance</MenuItem>
+                <MenuItem value="Marketing">Marketing</MenuItem>
+                <MenuItem value="UI Designing">UI Designing</MenuItem>
+                <MenuItem value="IT">IT</MenuItem>
+                <MenuItem value="Management">Management</MenuItem>
+                <MenuItem value="Business Developement">
+                  Business Developement
+                </MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl style={{ width: "22%" }} margin="normal" required>
+              <InputLabel>Available</InputLabel>
+              <Select
+                value={available}
+                name="available"
+                onChange={handleChange}
+              >
+                <MenuItem value={true}>Yes</MenuItem>
+                <MenuItem value={false}>No</MenuItem>
+              </Select>
+            </FormControl>
+            <button onClick={filterBy}>Filter Users</button>
+          </div>
+          <div className="search-by-name">
+            <TextField
+              style={{ width: "70%" }}
+              label="Name"
+              type="text"
+              name="name"
+              value={username}
+              onChange={handleChange}
+              margin="normal"
+              required
+            />
+            <button onClick={filterBy}>Search By Name</button>
+          </div>
+          <div className="all-users-container">
+            {allUsers &&
+              allUsers.users.map((user) => (
+                <UserDetailsCard key={user.u_id} user={user} />
+              ))}
+          </div>
+          <div className="toggle-btns">
+            <button id="prev" onClick={prevHandler}>
+              Prev Page
+            </button>
+            <button id="next" onClick={nextHandler}>
+              Next Page
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
